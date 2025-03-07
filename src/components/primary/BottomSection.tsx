aed21fda63b2d3c4f1cbfe778a00cf8f1d59f5f7
Heisemerie <ejike162@gmail.com>
2025-03-07 07:52:53 +0100

    Build bottom section

---
+++ src/components/primary/BottomSection.tsx
@@ -0,0 +1,13 @@
+import { Box, Center } from "@chakra-ui/react";
+
+const BottomSection = () => {
+  return (
+    <Box pb="2">
+      <Center fontSize={"xs"} color={"fg.muted"}>
+        ChatGPT can make mistakes. Check important info.
+      </Center>
+    </Box>
+  );
+};
+
+export default BottomSection;
