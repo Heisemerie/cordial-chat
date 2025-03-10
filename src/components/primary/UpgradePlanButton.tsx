04c7b73cf8a4f8ddf433a0a8f2b550008cd4e9fc
Heisemerie <ejike162@gmail.com>
2025-03-10 19:34:25 +0100

    Fix: restyle Upgrade plan button

--- src/components/primary/UpgradePlanButton.tsx
+++ src/components/primary/UpgradePlanButton.tsx
@@ -1,29 +1,32 @@
 import { UpgradeIcon } from "@/icons/sidebar-icons";
-import { Circle, HStack, Link, Stack, Text } from "@chakra-ui/react";
+import { Button, Circle, Stack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const UpgradePlanButton = () => {
   return (
-    <Link
-      href="#"
-      _hover={{ layerStyle: "fill.muted", textDecoration: "none" }}
-      py="2"
-      px="1"
-      h="10"
-      borderRadius="lg"
-      w="100%"
-      outline={"none"}
-    >
-      <HStack whiteSpace="nowrap">
+    <Link to={"#"}>
+      <Button
+        variant={"subtle"}
+        px="2"
+        py={"3.5"}
+        h="10"
+        borderRadius="lg"
+        w="100%"
+        whiteSpace="nowrap"
+        justifyContent={"flex-start"}
+      >
         <Circle size="8" fontSize="lg" borderWidth="1px">
           <UpgradeIcon />
         </Circle>
         <Stack gap="0" fontWeight="medium">
-          <Text fontSize="sm">Upgrade plan</Text>
+          <Text fontSize="sm" textAlign={"left"}>
+            Upgrade plan
+          </Text>
           <Text fontSize="xs" color="fg.subtle">
             More access to the best models
           </Text>
         </Stack>
-      </HStack>
+      </Button>
     </Link>
   );
 };
